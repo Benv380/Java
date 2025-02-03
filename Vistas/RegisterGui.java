ba6a21f3f6e359fba974ba9bdf36bc96404e6c08
Benjamin Marfull <167047801+Benv380@users.noreply.github.com>
2025-02-03 02:44:07 -0300

    Add files via upload

---
+++ Vistas/RegisterGui.java
@@ -0,0 +1,52 @@
+package Guis;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RegisterGui extends PrincipalGui{
+    public RegisterGui(){
+        super("register");
+    }
+
+    @Override
+    protected void addGuiComponents() {
+        JLabel bankAppLabel = new JLabel("Login Bank app");
+        bankAppLabel.setBounds(0,20,super.getWidth(), 40);
+        bankAppLabel.setFont(new Font("dialog", Font.BOLD, 33));
+        bankAppLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(bankAppLabel);
+
+        JLabel userNameLabel = new JLabel("UserName");
+        userNameLabel.setBounds(20,120, getWidth() - 30, 24);
+        userNameLabel.setFont(new Font("dialog", Font.PLAIN,20));
+        add(userNameLabel);
+
+        JTextField usernameFIeld = new JTextField();
+        usernameFIeld.setBounds(20,160, getWidth()- 50,40);
+        usernameFIeld.setFont(new Font("dialog", Font.PLAIN, 28));
+        add(usernameFIeld);
+
+        //passwords
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(20,220, getWidth() - 30, 24);
+        passwordLabel.setFont(new Font("dialog", Font.PLAIN,20));
+        add(passwordLabel);
+
+        JPasswordField passwordFIeld = new JPasswordField();
+        passwordFIeld.setBounds(20,260, getWidth()- 50,40);
+        passwordFIeld.setFont(new Font("dialog", Font.PLAIN, 28));
+        add(passwordFIeld);
+
+        JButton loginBtn = new JButton("loggin");
+        loginBtn.setBounds(20,470, getWidth() - 50, 40);
+        loginBtn.setFont(new Font("dialog", Font.BOLD,20));
+        add(loginBtn);
+
+        JLabel registerLabel = new JLabel("<html><a href= \"#\">Don't have an account? Regiter Here</a></html>");
+        registerLabel.setBounds(0,520, getWidth() - 10, 30);
+        registerLabel.setFont(new Font("dialog", Font.PLAIN,20));
+        registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(registerLabel);
+    }
+}
